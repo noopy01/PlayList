aee1ebecbc7da005caa9986556b68b4510a61af4
noopy01 <for.pharm01@gmail.com>
2024-07-01 14:18:06 +0900

    playlist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'; // Redirect 대신 Navigate를 사용
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/cart" element={<Cart />} />  
+          <Route path="/" element={<Home />} />  
+          <Route path="not-found" element={<NotFound />} />  
+          <Route path="*" element={<Navigate to="/not-found" />} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
